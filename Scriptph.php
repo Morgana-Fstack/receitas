1f78c66cbe32b3c5ea1e0217e10eb8db9f436af8
Morgana <morganapetterlecunha@gmail.com>
2026-08-18 12:34:07 -0300

    Remove unused legacy file Scriptph.php

--- Scriptph.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-http_response_code(410);
-header('Content-Type: application/json; charset=utf-8');
-
-echo json_encode([
-    'error' => 'Endpoint legado desativado por segurança.',
-], JSON_UNESCAPED_UNICODE);
